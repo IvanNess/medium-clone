e62cf99dc10fb90058c2f517dc3f81e3a403dd33
IvanNess <chai@bk.ru>
2020-09-30 14:57:55 +0300

    Updates

---
+++ precache-manifest.00575d66f8bce7c53e2e5395f7362d49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdbc4702f4618237c39de603ddc0a30",
+    "url": "/medium-clone/index.html"
+  },
+  {
+    "revision": "f631ff0d8cb2934deb10",
+    "url": "/medium-clone/static/js/2.7bac3ac9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medium-clone/static/js/2.7bac3ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee833ca8e7189ad9328",
+    "url": "/medium-clone/static/js/main.1ba74661.chunk.js"
+  },
+  {
+    "revision": "46f7af0fbebde0a0e41b",
+    "url": "/medium-clone/static/js/runtime-main.2a2926be.js"
+  }
+]);

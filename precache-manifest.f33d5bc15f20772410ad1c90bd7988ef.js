fbaff5ee47e6ba944d0c190ce5b16262f1e19586
IvanNess <chai@bk.ru>
2020-09-30 14:46:21 +0300

    Updates

---
+++ precache-manifest.f33d5bc15f20772410ad1c90bd7988ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d02d47abd1860b1a1c6f370fde3f43",
+    "url": "/medium-clone/index.html"
+  },
+  {
+    "revision": "f631ff0d8cb2934deb10",
+    "url": "/medium-clone/static/js/2.7bac3ac9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medium-clone/static/js/2.7bac3ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe0f5ce177c9d4b484a",
+    "url": "/medium-clone/static/js/main.397428dd.chunk.js"
+  },
+  {
+    "revision": "46f7af0fbebde0a0e41b",
+    "url": "/medium-clone/static/js/runtime-main.2a2926be.js"
+  }
+]);
